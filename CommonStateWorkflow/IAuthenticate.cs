403c3cf4f954861e5d7c78968d93813282e4effd
Sean D Smith <sdsmith@ropley.com>
2023-11-03 16:17:03 +0000

    Alternative simpler implementation added

---
+++ CommonStateWorkflow/IAuthenticate.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonStateWorkFlow;
+
+public interface IAuthenticate
+{
+    /// <summary>
+    /// Return the user name (official user name) if
+    /// authenticated, null if not
+    /// </summary>
+    /// <param name="userName">One of the user's login names</param>
+    /// <param name="password">The user's password</param>
+    /// <returns>The user's name if authenticated, null if not</returns>
+    public string? IsAuthenticatedUser(string userName, string password);
+}
---
+++ FsmWorkFlowTest/CommonStateTests.cs
@@ -0,0 +1,71 @@
+﻿using CommonStateWorkFlow;
+
+namespace FsmWorkFlowTest;
+
+[TestClass]
+public class CommonStateTests
+{
+    [TestMethod]
+    public void CanCreateFsm()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        Assert.IsInstanceOfType(fsm, typeof(FsmStateMachine));
+        Assert.IsNotNull(fsm.TestModel);
+    }
+
+    [TestMethod]
+    public void CanRunMainSequence()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        fsm.TestModel.TestRunId = 1;
+        fsm.SubmitTestRunForm();
+        fsm.Authenticate("fred", "fred");
+        fsm.Authenticate("joe", "joe");
+        Assert.AreEqual
+            (fsm.GetCurrentStateName(), "Showing Outcome");
+        Assert.AreEqual
+            ("Displaying test output!", fsm.TestModel.CompletionResult);
+        fsm.Terminate();
+        Assert.AreEqual(fsm.GetCurrentStateName(), "No State");
+    }
+
+    [TestMethod]
+    public void HandlesIncompleteFormFIlling()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        fsm.SubmitTestRunForm();
+        Assert.AreEqual(fsm.GetCurrentStateName(), "Filling Form");
+    }
+
+    [TestMethod]
+    public void HandlesFailedFirstAuth()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        fsm.TestModel.TestRunId = 1;
+        fsm.SubmitTestRunForm();
+        fsm.Authenticate("fred", "badpass");
+        Assert.AreEqual(fsm.GetCurrentStateName(), "Authenticating First Signer");
+    }
+
+    [TestMethod]
+    public void ResetsInputForm()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        fsm.TestModel.TestRunId = 1;
+        fsm.SubmitTestRunForm();
+        fsm.Authenticate("fred", "fred");
+        fsm.Start();
+        Assert.AreEqual(fsm.GetCurrentStateName(), "Filling Form");
+    }
+
+    [TestMethod]
+    public void TerminatesPrematurely()
+    {
+        FsmStateMachine fsm = new(new AuthenticationLib());
+        fsm.TestModel.TestRunId = 1;
+        fsm.SubmitTestRunForm();
+        fsm.Authenticate("fred", "fred");
+        fsm.Terminate();
+        Assert.AreEqual(fsm.GetCurrentStateName(), "No State");
+    }
+}
