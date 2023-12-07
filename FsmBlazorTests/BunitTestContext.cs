7dac6e2ccd40db7cf62c63df6116e26570b4ab53
Sean D Smith <sdsmith@ropley.com>
2023-12-07 13:57:50 +0000

    Added Bunit test samples to the solution to demonstrate test automation of the blazor workflow component.

---
+++ FsmBlazorTests/BunitTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bunit;
+
+namespace FsmBlazorTests;
+
+
+public abstract class BunitTestContext : TestContextWrapper
+{
+    [TestInitialize]
+    public void Setup() => TestContext = new Bunit.TestContext();
+
+    [TestCleanup]
+    public void TearDown() => TestContext?.Dispose();
+}
---
+++ FsmBlazorTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FsmBlazorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
